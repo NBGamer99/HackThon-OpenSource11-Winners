a9f08193c2869223422fbe0c573efd7f6e74bb6a
NBGamer99 <nabouziyasser80@gmail.com>
2023-05-08 15:36:37 +0100

    add app

---
+++ app/wa3ta9/wa3ta9/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wa3ta9 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wa3ta9.settings')
+
+application = get_asgi_application()
